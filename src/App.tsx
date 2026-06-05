0a8ab8c2ec453aa0ad20854cac3b38688be8b223
unknown <derickgarcia1212@gmail.com>
2026-06-05 20:57:38 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <html>
+      <body>
+        Hello World
+      </body>
+    </html>
+   
+  )
+}
+
+export default App
