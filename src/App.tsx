f77c6f7dc71e7f08bd4fd693abd41680550ebd1f
JamesGuinto26 <james_guinto@dlsu.edu.ph>
2026-06-08 11:27:11 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from "./components/Navbar";
+import Scheduling from "./pages/Scheduling";
+
 
 function App() {
-  
-
   return (
-    <html>
-      <body>
-        Hello World
-      </body>
-    </html>
-   
-  )
+    <div>
+      <Navbar />
+      
+      <main className="main-content">
+        <Scheduling />
+      </main>
+    </div>
+  );
 }
 
 export default App
+
