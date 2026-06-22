7b826867893e8a2b2d5af0f32d788f6293b81b87
Cj Manaois <chriscel_manaois@dlsu.edu.ph>
2026-06-22 16:47:56 +0800

    Sign in & Sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import './App.css'
 import LandingPage from "./pages/LandingPage";
-import Navbar from "./components/Navbar";
 import Scheduling from "./pages/Scheduling";
 import PrivacySection from './pages/PrivacySection';
 import TermsOfService from './pages/TermsOfService';
 import NotFoundPage from './pages/NotFound';
 import CreatePost from './pages/CreatePost';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { StrictMode } from 'react';
 const router = createBrowserRouter([
-  {path: "/", element: <LandingPage />},
-  {path:"/dashboard", element:<Scheduling></Scheduling>},
-  {path:"/create-post", element:<CreatePost/>},
-  {path:"/privacy", element:<PrivacySection/>},
-  {path:"/terms", element:<TermsOfService/>},
-  {path:"*", element:<NotFoundPage/>}
+  { path: "/", element: <LandingPage /> },
+  { path: "/signin", element: <SignIn /> },
+  { path: "/signup", element: <SignUp /> },
+  { path: "/app", element: <SignIn /> },
+  { path: "/dashboard", element: <Scheduling /> },
+  { path: "/create-post", element: <CreatePost /> },
+  { path: "/privacy", element: <PrivacySection /> },
+  { path: "/terms", element: <TermsOfService /> },
+  { path: "*", element: <NotFoundPage /> },
 ])
 //Routing 
 function App() {
